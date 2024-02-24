62ff7336b06f26e4cad4c52c66b85fe4c26d5e97
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-02-25 01:42:48 +0300

    Update BurcBulanProgram.java

--- src/BurcBulanProgram.java
+++ src/BurcBulanProgram.java
@@ -24,77 +24,77 @@ public class BurcBulanProgram {
 	    
 	    if (ay == 2) 
     	{
-	    	if (gun >= 19 && gun <= 31) System.out.println("Kova burcusunuz.");
-	    	else if (gun>=20 && gun<=31) System.out.println("Balık burcusunuz.");  
+	    	if (gun >= 1 && gun <= 19) System.out.println("Kova burcusunuz.");
+	    	else if (gun>=20 && gun<=29) System.out.println("Balık burcusunuz.");  
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	
 	    if (ay == 3) 
     	{
-	    	if (gun >= 20 && gun <= 21) System.out.println("Balık burcusunuz.");
+	    	if (gun >= 1 && gun <= 20) System.out.println("Balık burcusunuz.");
 	    	else if (gun>=21 && gun<=31) System.out.println("Koç burcusunuz.");  	
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 4) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 20) System.out.println("Koç burcusunuz.");
+	    	else if (gun>=21 && gun<=30) System.out.println("Boğa burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 5) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 21) System.out.println("Boğa burcusunuz.");
+	    	else if (gun>=22 && gun<=31) System.out.println("ikizler burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 6) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz.");
+	    	if (gun >= 1 && gun <= 22) System.out.println("İkizler burcusunuz.");
+	    	else if (gun>=23 && gun<=30) System.out.println("Yengeç burcusunuz.");
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 7) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz.");  
+	    	if (gun >= 1 && gun <= 22) System.out.println("Yengeç burcusunuz.");
+	    	else if (gun>=23 && gun<=31) System.out.println("Aslan burcusunuz.");  
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 8) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 22) System.out.println("Aslan burcusunuz.");
+	    	else if (gun>=23 && gun<=31) System.out.println("Başak burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
 	    	
 	    if (ay == 9) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 22) System.out.println("Başak burcusunuz.");
+	    	else if (gun>=23 && gun<=30) System.out.println("Terazi burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 10) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 22) System.out.println("Terazi burcusunuz.");
+	    	else if (gun>=23 && gun<=31) System.out.println("Akrep burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 11) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 21) System.out.println("Akrep burcusunuz.");
+	    	else if (gun>=22 && gun<=30) System.out.println("Yay burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	    
 	    if (ay == 12) 
     	{
-	    	if (gun >= 19 && gun <= 21) System.out.println("Oğlan burcusunuz.");
-	    	else if (gun>=22 && gun<=31) System.out.println("Kova burcusunuz."); 
+	    	if (gun >= 1 && gun <= 21) System.out.println("Yay burcusunuz.");
+	    	else if (gun>=22 && gun<=31) System.out.println("Oğlak burcusunuz."); 
 	    	else System.out.println("Geçerli bir gün giriniz.");
     	}
 	}
